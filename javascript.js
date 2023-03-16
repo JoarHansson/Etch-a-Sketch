a109f5dd8477cecfe445a0537589674a5a13b2ad
DJ-Juarez <124060119+DJ-Juarez@users.noreply.github.com>
2023-03-16 11:29:23 +0100

    make size of grid container static and number of grid items flexible
    Number of grid items is defined with prompt input as of now. Also,
    add mouseover event to change color of grid items to a random one.

--- javascript.js
+++ javascript.js
@@ -8,5 +8,20 @@ for (i = 0; i < numInput ** 2; i++) {
     gridContainer.appendChild(square)
 }
 
-gridContainer.style.gridTemplateColumns = `repeat(${numInput}, 15px [test])`;
-gridContainer.style.gridTemplateRows = `repeat(${numInput}, 15px [test])`;
+const containerWidth = 960;
+gridContainer.style.width = `${containerWidth}px`
+gridContainer.style.gridTemplateColumns = `repeat(${numInput}, ${containerWidth/numInput}px [test])`;
+gridContainer.style.gridTemplateRows = `repeat(${numInput}, ${containerWidth/numInput}px [test])`;
+
+
+gridContainer.addEventListener("mouseover", (event) => {
+    if (event.target.classList.contains("square")) {
+        let randomNumber1 = Math.floor(Math.random() * 255);
+        let randomNumber2 = Math.floor(Math.random() * 255);
+        let randomNumber3 = Math.floor(Math.random() * 255);
+        event.target.style.backgroundColor = `rgb(${randomNumber1},${randomNumber2},${randomNumber3})`;
+    }
+});
+
+// console.log(randomNumber);
+
